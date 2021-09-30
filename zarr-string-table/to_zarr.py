3ed7ab894f782b7af748e57484adf66bb71bfedd
Constantin Pape <constantin.pape@embl.de>
2021-09-30 14:02:44 +0200

    Change to gzip compression

--- zarr-string-table/to_zarr.py
+++ zarr-string-table/to_zarr.py
@@ -3,10 +3,11 @@ import pandas as pd
 import numpy as np
 import zarr
 
+from numcodecs import GZip
+
 
 tab = pd.read_csv("../spots_reduced.csv")
 col = np.array([str(v) for v in tab["target"].values])
-print(np.unique(col))
 
 f = zarr.open("table.zarr")
-f.create_dataset("fixed-length", data=col, chunks=(2048,))
+f.create_dataset("fixed-length", data=col, chunks=(2048,), compressor=GZip())
