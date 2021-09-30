1f21fd02337bba392285aa7235aec85c042ce51a
Constantin Pape <constantin.pape@embl.de>
2021-09-30 14:16:47 +0200

    Add zarr float array

--- zarr-string-table/to_zarr.py
+++ zarr-string-table/to_zarr.py
@@ -6,8 +6,19 @@ import zarr
 from numcodecs import GZip
 
 
-tab = pd.read_csv("../spots_reduced.csv")
-col = np.array([str(v) for v in tab["target"].values])
+def fixed_length():
 
-f = zarr.open("table.zarr")
-f.create_dataset("fixed-length", data=col, chunks=(2048,), compressor=GZip())
+    tab = pd.read_csv("../spots_reduced.csv")
+    col = np.array([str(v) for v in tab["target"].values])
+
+    f = zarr.open("table.zarr")
+    f.create_dataset("fixed-length", data=col, chunks=(2048,), compressor=GZip())
+
+
+def float_reference():
+    data = np.random.rand(2048).astype("float32")
+    f = zarr.open("table.zarr")
+    f.create_dataset("float", data=data, chunks=(512,), compressor=GZip())
+
+
+float_reference()
