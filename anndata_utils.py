bdb88212e2834733055430609b4207ff3e6ce331
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2021-09-30 14:07:48 +0200

    fix anndata tests

--- anndata_utils.py
+++ anndata_utils.py
@@ -1,9 +1,21 @@
 import os
+from typing import Dict, Tuple, Union
 
 import anndata
+import pandas as pd
 
 
-def create_anndata_from_csv(fpath: os.PathLike) -> anndata.AnnData:
+DENSE_COLUMNS = [
+    'zc',
+    'yc',
+    'xc'
+]
+
+
+def setup_anndata(
+        fpath: os.PathLike,
+        out_dir: os.PathLike
+) -> Tuple[Tuple[()], Dict[str, Union[anndata.AnnData, os.PathLike]]]:
     """Create the anndata object from the example csv
 
     Parameters
@@ -13,5 +25,27 @@ def create_anndata_from_csv(fpath: os.PathLike) -> anndata.AnnData:
 
     Returns
     -------
-    ann : anndata.AnnData
+    benchmark_args : Dict[str, Union[anndata.AnnData, os.PathLike]]
+        The input arguments for ann_to_zarr. 'ann_obj' is the
+        AnnData object to write, 'out_path' is the file path
+        to write the zarr to.
     """
+    df = pd.read_csv(fpath)
+
+    # get the dense array
+    dense_array = df[DENSE_COLUMNS].to_numpy()
+
+    # drop the dense array from the table
+    obs = df.drop(DENSE_COLUMNS, axis='columns')
+
+    # create the AnnData object
+    ann_obj = anndata.AnnData(X=dense_array, obs=obs)
+
+    # make the filepath
+    out_path = os.path.join(out_dir, 'test.zarr')
+
+    return (), {'ann_obj': ann_obj, 'out_path': out_path}
+
+
+def ann_to_zarr(ann_obj: anndata.AnnData, out_path: os.PathLike):
+    ann_obj.write_zarr(out_path)
