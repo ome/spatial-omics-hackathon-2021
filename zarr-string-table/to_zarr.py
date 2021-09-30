b86be66c67c19d734beb6ec3064781ed5b0f131d
Constantin Pape <constantin.pape@embl.de>
2021-09-30 15:40:27 +0200

    Add string column with random data

--- zarr-string-table/to_zarr.py
+++ zarr-string-table/to_zarr.py
@@ -1,3 +1,6 @@
+import random
+import string
+
 import pandas as pd
 
 import numpy as np
@@ -7,12 +10,26 @@ from numcodecs import GZip
 
 
 def fixed_length():
-
     tab = pd.read_csv("../spots_reduced.csv")
     col = np.array([str(v) for v in tab["target"].values])
 
     f = zarr.open("table.zarr")
     f.create_dataset("fixed-length", data=col, chunks=(2048,), compressor=GZip())
+
+
+def fixed_length_random():
+
+    def rand_string(mlen):
+        letters = string.ascii_lowercase
+        slen = np.random.randint(2, 10)
+        return "".join(random.choice(letters) for _ in range(slen))
+
+    col = np.array(
+        [rand_string(10) for _ in range(2048)]
+    )
+
+    f = zarr.open("table.zarr")
+    f.create_dataset("fixed-length-random", data=col, chunks=(512,), compressor=GZip())
 
 
 def float_reference():
@@ -21,4 +38,5 @@ def float_reference():
     f.create_dataset("float", data=data, chunks=(512,), compressor=GZip())
 
 
-float_reference()
+# float_reference()
+fixed_length_random()
